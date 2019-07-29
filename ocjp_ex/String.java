d1a10ca00f7dda1e6316e14e337e183c46723181
BCSJH <39579101+BCSJH@users.noreply.github.com>
2019-07-29 20:18:09 +0900

    ocjp_2019-02-15_33
    2019-02-15_33

---
+++ ocjp_ex/String.java
@@ -0,0 +1,14 @@
+package ocjp;
+import java.util.*;
+import java.lang.*;
+public class h {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			String str  = " ";
+			str.trim();
+			System.out.println("음"+str.trim()+"음");
+			System.out.println(str.equals("") + " " + str.isEmpty());
+	}
+
+}
