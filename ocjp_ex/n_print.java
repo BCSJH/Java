8b58f396d2e569025ee5c81eac61ab748e6c4cc5
BCSJH <39579101+BCSJH@users.noreply.github.com>
2019-07-30 15:35:04 +0900

    ocjp_2019-02-15_49
    오류

---
+++ ocjp_ex/n_print.java
@@ -0,0 +1,16 @@
+package ocjp;
+import java.util.*;
+import java.lang.*;
+
+public class h {
+	public static void main(String[] args) {
+		
+		int n[][] = {{1,3},{2,4}};
+		for(int i = n.length-1;i>=0;i--) {
+			for(int y:n[i]) {
+				System.out.print(y);//2413
+			}
+		}
+		
+		}
+}
