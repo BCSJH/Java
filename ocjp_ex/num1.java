882494eae32987b4a37630d4b9b267cb1f348c62
BCSJH <39579101+BCSJH@users.noreply.github.com>
2019-07-30 15:09:44 +0900

    ocjp_2019-02-15_46

---
+++ ocjp_ex/num1.java
@@ -0,0 +1,19 @@
+package ocjp;
+import java.util.*;
+import java.lang.*;
+
+public class h {
+	public static void main(String[] args) {
+		
+		int num1[] = new int[3];
+		int num2[] = {1,2,3,4,5};
+		num1 = num2;
+		
+		for (int x : num2) {
+			System.out.print(x + " : ");
+		}
+		for (int x : num1) {
+			System.out.print(x + " : ");
+		}
+		}
+}
