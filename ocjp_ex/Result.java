bddc9cde65e781263faeb7c87c86308b70cd011c
BCSJH <39579101+BCSJH@users.noreply.github.com>
2019-07-30 16:05:08 +0900

    ocjp_2019-02-15_60
    오류

---
+++ ocjp_ex/Result.java
@@ -0,0 +1,12 @@
+package ocjp;
+import java.util.*;
+import java.lang.*;
+
+public class h {
+	public static void main(String[] args) {
+		
+		System.out.println("Result A "+ 0 + 1);
+		System.out.println("Result B "+ (1) + (2));
+		
+		}
+}
