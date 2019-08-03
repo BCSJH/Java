bdcc2edb02c6dc18fe9323b230d76e2dc712c241
BCSJH <39579101+BCSJH@users.noreply.github.com>
2019-08-03 09:38:14 +0900

    ocjp_2019-02-15_63
    2019-02-15_63
    오류가 떠야되는데 실행 잘 됨..
    [1,2,4]

---
+++ ocjp_ex/points_add_remove.java
@@ -0,0 +1,17 @@
+package ocjp;
+import java.util.*;
+import java.lang.*;
+
+public class h {
+	public static void main(String[] args) {
+		ArrayList<Integer> points = new ArrayList();
+		points.add(1);
+		points.add(2);
+		points.add(3);
+		points.add(4);
+		points.add(null);
+		points.remove(2);
+		points.remove(null);
+		System.out.println(points);
+		}
+}
