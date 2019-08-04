a67378542cd8c432a042155072f5bf2c3ea8de21
BCSJH <39579101+BCSJH@users.noreply.github.com>
2019-08-05 00:02:04 +0900

    ocjp_2019-02-15_72
    2019-02-15_72

---
+++ ocjp_ex/args_JavaEE.java
@@ -0,0 +1,19 @@
+package ocjp;
+import java.util.*;
+import java.lang.*;
+
+public class h {
+	public static final int MIN = 1;
+	public static void main(String[] args) {
+			int x = args.length;
+			System.out.println(x);
+			if(checkLimit(x)) {
+				System.out.println("Java SE");	
+			}else {
+				System.out.println("Java EE");
+			}
+			}
+			public static boolean checkLimit(int x) {
+				return (x>=MIN)?true:false;
+			}
+}
