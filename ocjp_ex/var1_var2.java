1736a88abdfb79493303f56b83e31446672e1f38
BCSJH <39579101+BCSJH@users.noreply.github.com>
2019-08-05 01:16:27 +0900

    ocjp_2019-02-15_74
    답이 13480.0로 되어있는데 실행해보면 결과는 13460.0로 나온다.

---
+++ ocjp_ex/var1_var2.java
@@ -0,0 +1,11 @@
+package ocjp;
+import java.util.*;
+import java.lang.*;
+
+public class h {
+	public static void main(String[] args) {
+		float var1 = (12_345.01 >=123_45.00) ? 12_456 : 124_56.02f;
+		float var2 = var1 + 1004;
+		System.out.print(var2);
+	}
+}
