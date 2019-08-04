a89f5554445f583b052d93a27891fa0ff9baba0d
BCSJH <39579101+BCSJH@users.noreply.github.com>
2019-08-04 23:40:40 +0900

    ocjp_2019-02-15_68
    답이 An exception is thrown at runtime로 적혀있는데
    실행해보면 5가 나옴

---
+++ ocjp_ex/myStr_indexof.java
@@ -0,0 +1,12 @@
+package ocjp;
+import java.util.*;
+import java.lang.*;
+
+public class h {
+	public static void main(String[] args) {
+			String myStr = "Hello World";
+			myStr.trim();
+			int il = myStr.indexOf(" ");
+			System.out.println(il);
+		}
+}
