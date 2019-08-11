61cf035657ae1de6b02a6cfbf45d9248f99c93a4
BCSJH <39579101+BCSJH@users.noreply.github.com>
2019-08-11 23:36:08 +0900

    ocjp_2019-02-15_79
    2019-02-15_79

---
+++ ocjp_ex/int_i_j_k.java
@@ -0,0 +1,12 @@
+package ocjp;
+import java.util.*;
+import java.lang.*;
+
+public class h {
+	public static void main(String[] args) {
+		int i = 10;
+		int j = 20;
+		int k = j +=i/5;
+		System.out.println(i + ":"+j+":"+k);
+	}
+}
