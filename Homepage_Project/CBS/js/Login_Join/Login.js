89a50653324cbc8b32e2805c60d9dd63f2442d5b
BCSJH <39579101+BCSJH@users.noreply.github.com>
2021-08-28 22:57:56 +0900

    frame 추가
    Commit
    - CBS Frame  추가
    - Need
    - 자기소개 frame 추가

---
+++ Homepage_Project/CBS/js/Login_Join/Login.js
@@ -0,0 +1,9 @@
+function login(){
+    var id = document.getElementById('userId');
+    var idcheck = document.getElementById('idcheck');
+    idcheck.innerText = id;
+}
+
+function loginData(id, pwd){
+
+}
