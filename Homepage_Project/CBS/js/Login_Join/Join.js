9aaff72230679177ac61ede61a10d07900621475
BCSJH <39579101+BCSJH@users.noreply.github.com>
2021-09-21 01:26:39 +0900

    기능 구현
    1. 로그인
    2. 아이디 찾기
    3. 비밀번호 메일 연동
    4. css 적용
    5. 관리자 페이지 기능 구현 ( 회원 출력, 음식 완료 체크, 매출 출력 기능)
    6. 사용자 페이지 기능 구현 ( 음식 구매, 이용권 구매, 회원 비밀번호 수정 기능)
    
    + 메일 적용 마무리

--- Homepage_Project/CBS/js/Login_Join/Join.js
+++ Homepage_Project/CBS/js/Login_Join/Join.js
@@ -1,28 +1,17 @@
-const user = function(userid, password1, password2, name, phone, email){
+const user = function(userid, password1, username, phone, email, reg_Date, mu){
     this.userid = userid;
     this.password1 = password1;
-    this.password2 = password2;
-    this.name = name;
+    this.username = username;
     this.phone = phone; 
-    this. email = email;
-}
+    this.email = email;
+    this.reg_Date = reg_Date;
+    this.mu = mu;
+};
 
 
-
-//필수 입력값, 이메일 주소 체크
-const info_join_success = (joinform) => {
-    let span_red_check = joinform.getElementsByTagName("span");
-    // console.log(span_red_check);
-    for(let info = 0; info < 6; info++){
-        if(span_red_check[info].innerHTML == ""){
-            return 1;
-        }
-    }
-
-    // (1) 성공시 값 txt 값 저장
-    // (2) 페이지 이동
-    console.log("성공");
-    return 0;
+//새로고침
+function func_reaload(){
+    parent.location.reload();
 }
 
 
@@ -48,23 +37,19 @@ const email_check = (email) => {
 //입력 태그 체크
 const info_space_check = (joinform) => {
     let check =  joinform.getElementsByTagName("input");
-    let cnt = 0;
     for(let info = 0; info < 6; info++){
         join_info_check(check[info]);
     }
 }
 
-
-//입력 공백 체크
+//입력 체크
 const join_info_check = (info) => {
-    let idReg = /[a-zA-Z0-9]{4,19}/g;
+    let idReg = /[a-zA-Z0-9]{5,19}/g;
     let phoneReg = /[0][1][(0-9)]{8,9}$/;
     let pwdReg = /^(?=.*?[A-Za-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,15}$/g;
     let infomsg = info.id + '_msg';
 
     let password1 = document.getElementById("password1");
-    console.log(infomsg);
-    console.log(info.id);
     if(info.value == ""){
         if(info.id !== "email"){
             document.getElementById(infomsg).innerHTML = "필수 정보입니다.";
@@ -72,8 +57,14 @@ const join_info_check = (info) => {
             document.getElementById(infomsg).innerHTML = "";
         }
     }else{// 뭔가 입력이 되어있다면.
-        if(info.id == "userid" && !idReg.test(info.value)){
-            document.getElementById(infomsg).innerHTML = "5~20자의 영문, 숫자만 사용 가능합니다.";
+        if(info.id == "userid"){
+            if(!idReg.test(info.value))
+                document.getElementById(infomsg).innerHTML = "5~20자의 영문, 숫자만 사용 가능합니다.";
+            else if(!id_check(info.value)){
+                document.getElementById(infomsg).innerHTML = "이미 존재하는 아이디입니다.";
+            }else{
+                document.getElementById(infomsg).innerHTML = "";
+            }    
         }else if(info.id == "password1" && !pwdReg.test(info.value)){
             document.getElementById(infomsg).innerHTML = "숫자/문자/특수문자가 포함된 8-15자리 이내의 암호로 구성되어야 합니다.";
         }else if(info.id == "password2" && password1.value != info.value){
@@ -92,16 +83,44 @@ const join_info_check = (info) => {
     }
 }
 
+function id_check(chk_id){
+    //값 불러오기
+    const entries = JSON.parse(localStorage.getItem('info_user')) || [];
+    for(let i = 0; i < entries.length; i++){
+        const userid = $(entries)[i].userid;
+        if(chk_id == userid){
+            return false;
+        }
+    }
+    return true;
+};
+
+const info_save = (joinform) => {
+    //1. localStorage에 저장
+    let today = new Date();   
+
+    let year = today.getFullYear(); // 년도
+    let month = today.getMonth() + 1;  // 월
+    let date = today.getDate();  // 날짜
+    const entry = new user(joinform.userid.value, joinform.password1.value, joinform.username.value, joinform.phone.value, joinform.email.value, year+"/"+month +"/"+date ,'user');
+
+
+    const entries = JSON.parse(localStorage.getItem('info_user')) || [];
+    entries.push(entry);
+
+    localStorage.setItem('info_user',JSON.stringify(entries));
+
+};
+
 
 
 function join(joinform){
-    console.log("도착");
+    // console.log("도착");
     // console.log(joinform);
     // console.log(joinform.new_userId.value);
-    console.log(joinform);
+    // console.log(joinform);
 
     // pwdck(joinform.password1, joinform.password2); // 비밀번호 확인
     info_space_check(joinform); // 입력 공백 체크
     // info_join_success(joinform);
-    const a = new user(joinform.id.value, joinform.password1.value, joinform.password2.value, joinform.name.value, joinform.phone.value, joinform.email.value);
 }
